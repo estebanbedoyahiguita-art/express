c27f4cd2adf85d43c6ab988e68978ef7241a2641
Esteban bedoya <estebanbedoyahiguita@gmail.com>
2025-08-22 22:07:03 -0500

    config url

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 3000;
 
@@ -13,6 +15,22 @@ app.get('/', (req, res) =>{
     `);
 });
 
-app.listen(PORT, ()=>{
-    console.log(`Servidor: http://localhost:${PORT}`);
+app.get('/users/:id', (req, res) =>{
+    const userId= req.params.id;
+    res.send(`Mostrar la información del usuario con ID: ${userId}`);
 });
+
+app.get('/search', (req, res) =>{
+    const terms = req.query.termino || 'No especificado';
+    const category = req.query.categoria || 'Todas';
+
+    res.send(`
+        <h2>Resultados de Busqueda: <h2>
+        <p> Termino: ${terms}<p>
+        <p>Categoria: ${category}<p>
+    `)
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Servidor:http://localhost:${PORT}`);
+}); 
